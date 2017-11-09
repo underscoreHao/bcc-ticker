cf3f91070760b1b5cf3ea7369653afcfc2d4c410
Pavel Danov <_hao@ensifer.io>
2017-11-09 13:27:48 +0200

    Added default values and the ability to change the desired currency. Did some refactoring.

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,59 +1,79 @@
-chrome.runtime.onStartup.addListener(function(){
-	BitConnect.setupExtension();
-});
+(function() {
+	var defaultVals = {
+		'refresh_time': 20000,
+		'green_color': "#7ED321",
+		'red_color': "#D0021B",
+		'currency': "USD"
+	};
 
-chrome.runtime.onInstalled.addListener(function(){
-	BitConnect.setupExtension();
-});
+	var config = {};
 
-chrome.alarms.onAlarm.addListener(function(alarm) {
-	if (alarm.name == chrome.runtime.getManifest().name) {
-		BitConnect.getCurrentExchangeRate();
-	}
-  });
+	var BitConnect = {
+		init: function() {
+			this.resetCurrentVals();
+			this.initRequest();
+		},
 
-var BitConnect = {
-	setupExtension: function(receiver) {
-		chrome.browserAction.setBadgeBackgroundColor({color: [0,0,0,128]});
-		this.startRequesting();
-	},
-
-	startRequesting: function() {
-		this.getCurrentExchangeRate();
-		var self = this;
-		this.globalIntervalId = window.setInterval(function() {
-			self.getCurrentExchangeRate();
-		}, 20000);
-	},
-
-	getCurrentExchangeRate: function() {
-		var request = new XMLHttpRequest();
-		if (request == null) {
-			console.error("Unable to create request!");
-		} else {
-			request.onreadystatechange = function() {
-				if (request.readyState == 4) {
-					BitConnect.handleResponse(request.responseText);
-				}
+		resetCurrentVals: function() {
+			for (var key in defaultVals) {
+				config[key] = localStorage[key] || defaultVals[key];
 			}
+		},
+	
+		initRequest: function() {
+			this.getCurrentExchangeRate();
+			var self = this;
+			this.globalIntervalId = window.setInterval(function() {
+				self.getCurrentExchangeRate();
+			}, config.refresh_time);
+		},
+	
+		getCurrentExchangeRate: function() {
+			var request = new XMLHttpRequest();
+			if (request == null) {
+				console.error("Unable to create request!");
+			} else {
+				request.onreadystatechange = function() {
+					if (request.readyState == 4) {
+						BitConnect.handleResponse(request.responseText);
+					}
+				}
+	
+				var url;
 
-			var url = "https://api.coinmarketcap.com/v1/ticker/bitconnect/";
-			request.open("GET", url, true);
-			request.send(null);
+				if (config.currency === "USD")
+				{
+					url = "https://api.coinmarketcap.com/v1/ticker/bitconnect/";					
+				}
+				else
+				{
+					url = "https://api.coinmarketcap.com/v1/ticker/bitconnect/?convert=" + config.currency;
+				}
+
+				request.open("GET", url, true);
+				request.send(null);
+			}
+		},
+	
+		handleResponse: function(response) {
+			if (response.length == 0) {
+				setBadge("---");
+			} else {
+				var results = JSON.parse(response);
+				var current_currency = "price_" + config.currency.toLowerCase();
+				var current_rate = Math.round(results[0][current_currency]).toString();
+				var percent_change = (results[0]["percent_change_1h"]).toString();
+				BitConnect.setBadge(current_rate, percent_change);
+			}
+		},
+	
+		setBadge: function(current_rate, percent_change) {
+			var color = Math.sign(percent_change) ? config.red_color : config.green_color;
+			chrome.browserAction.setBadgeBackgroundColor({color: color});
+			chrome.browserAction.setBadgeText({text: current_rate});
 		}
-	},
-
-	handleResponse: function(response) {
-		if (response.length == 0) {
-			setBadge("---");
-		} else {
-			var results = JSON.parse(response);
-			var current_rate = Math.round(results[0].price_usd).toString();
-			BitConnect.setBadge(current_rate);
-		}
-	},
-
-	setBadge: function(current_rate) {
-		chrome.browserAction.setBadgeText({text: current_rate});
 	}
-}
+
+	return BitConnect;
+	
+})().init();
